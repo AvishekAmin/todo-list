75e0da73df7c27907c9538f7f917e4e33f913420
Avishek Amin <avishekamin207@gmail.com>
2026-05-31 21:12:20 +0530

    Mark all done and delete all feature

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,19 @@
 import { useSelector } from "react-redux";
 import AddForm from "./AddForm";
 import { useDispatch } from "react-redux";
-import { deleteTodo, markAsDone } from "../features/todo/todoSlice";
+import {
+  deleteTodo,
+  markAsDone,
+  markAllDone,
+  deleteAll,
+} from "../features/todo/todoSlice";
 
 export default function Todo() {
   const todos = useSelector((state) => state.todos);
-  console.log(todos);
-
+  const allDone = todos.every((todo) => todo.isDone);
   const dispatch = useDispatch();
 
-  const clickHandler = (id) => {
+  const deleteTodoHandler = (id) => {
     dispatch(deleteTodo(id));
   };
 
@@ -17,25 +21,34 @@ export default function Todo() {
     dispatch(markAsDone(id));
   };
 
+  const markAllDoneHandler = () => {
+    dispatch(markAllDone());
+  };
+
+  const deleteAllHandler = () => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete all tasks?",
+    );
+
+    if (confirmDelete) {
+      dispatch(deleteAll());
+    }
+  };
+
   return (
     <div className="container">
       <h2 className="heading">Todo List App</h2>
-
+      <p className="counter">
+        Completed: {todos.filter((todo) => todo.isDone).length} / {todos.length}
+      </p>
       <AddForm />
-
       {todos.length === 0 ? (
         <p className="empty-message">Add your first task</p>
       ) : (
         <ol className="task-list">
           {todos.map((todo) => (
             <li key={todo.id} className="task-item">
-              <span
-                className="task-text"
-                style={{
-                  textDecoration: todo.isDone ? "line-through" : "none",
-                  opacity: todo.isDone ? 0.6 : 1,
-                }}
-              >
+              <span className={`task-text ${todo.isDone ? "completed" : ""}`}>
                 {todo.task}
               </span>
 
@@ -48,13 +61,32 @@ export default function Todo() {
 
               <button
                 className="delete-btn"
-                onClick={() => clickHandler(todo.id)}
+                onClick={() => deleteTodoHandler(todo.id)}
               >
                 Delete
               </button>
             </li>
           ))}
         </ol>
+      )}
+      {todos.length > 0 && (
+        <div style={{ marginTop: "20px" }}>
+          <button
+            className="done-btn"
+            onClick={markAllDoneHandler}
+            disabled={allDone}
+          >
+            Mark All Done
+          </button>
+
+          <button
+            className="delete-btn"
+            onClick={deleteAllHandler}
+            style={{ marginLeft: "10px" }}
+          >
+            Delete All
+          </button>
+        </div>
       )}
     </div>
   );
--- src/features/todo/todoSlice.jsx
+++ src/features/todo/todoSlice.jsx
@@ -22,16 +22,24 @@ export const todoSlice = createSlice({
     },
 
     markAsDone: (state, action) => {
-      const todo = state.todos.find(
-        (todo) => todo.id === action.payload
-      );
+      const todo = state.todos.find((todo) => todo.id === action.payload);
 
       if (todo) {
         todo.isDone = !todo.isDone;
       }
     },
+
+    markAllDone: (state) => {
+      state.todos.forEach((todo) => {
+        todo.isDone = true;
+      });
+    },
+
+    deleteAll: (state) => {
+      state.todos = [];
+    },
   },
 });
 
-export const { addTodo, deleteTodo, markAsDone } = todoSlice.actions;
+export const { addTodo, deleteTodo, markAsDone, markAllDone, deleteAll } = todoSlice.actions;
 export default todoSlice.reducer;
